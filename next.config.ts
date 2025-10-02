49f4f92d7cd49337d6ddb1d044eb3b9d91812612
abdeliah <158301281+kanata-kan@users.noreply.github.com>
2025-10-02 15:39:51 +0100

    Fix/new bag (#20)
    * fix bag gallery fetch
    
    * fix bag fetch cars an activities page
    
    * fix bag 3
    
    * fix bag (#13)
    
    * Bugfix/vercel invalid url (#11)
    
    * fix: replace relative fetch /api/activities with static JSON for Vercel build
    
    * fix: replace relative fetch /api/activities with static JSON for Vercel build 2
    
    * fix bag gallery fetch (#12)
    
    * fix bag pages fetch 2
    
    * fix bag fetch 3
    
    * fix a bug FOUC + add Registry file
    
    * new update fix FOUS

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    styledComponents: true, 
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
   },
 };
-
 export default nextConfig;
-
-
