cc02d24b3fb5b71c4d353a76c9e311f360365664
Abdelilah Wajid <kanata10kan@gmail.com>
2025-10-02 15:13:11 +0100

    fix a bug FOUC + add Registry file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    styledComponents: {
-      displayName: true, // يسهل debug فالـDevTools
-      ssr: true, // يحل مشاكل FOUC (flash of unstyled content)
-    },
+    styledComponents: true, // هادي وحدها كافية
   },
 };
 
