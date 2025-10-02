c0de94cc35f1152fc9cde724139465dde4539364
abdeliah <158301281+kanata-kan@users.noreply.github.com>
2025-10-02 14:53:13 +0100

    Fix/bag (#17)
    * fix bag gallery fetch
    
    * fix bag fetch cars an activities page
    
    * fix bag 3
    
    * fix bag (#13)
    
    * Bugfix/vercel invalid url (#11)
    
    * fix: replace relative fetch /api/activities with static JSON for Vercel build
    
    * fix: replace relative fetch /api/activities with static JSON for Vercel build 2
    
    * fix bag gallery fetch (#12)
    
    * fix bag pages fetch 2
    
    * fix bag fetch 3
    
    * Create .babelrc
    
    Add babel rc config
    
    * Delete .babelrc
    
    * Create registry.tsx
    
    * Rename registry.tsx to lib/registry.tsx
    
    * Update layout.tsx
    
    * Update next.config.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,14 @@ import Navbar from "@/components/ui/Navbar";
 import { ThemeProviderCustom } from "@/hooks/useThemeToggle";
 import { baseMetadata } from "@/lib/metadata/base";
 import { Geist, Geist_Mono } from "next/font/google";
+import { StyledComponentsRegistry } from "@/lib/registry";
 
-const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
+const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-geist-mono",
 });
 
-// ✅ Metadata site-wide
 export const metadata = baseMetadata;
 
 export const viewport = {
@@ -37,13 +33,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProviderCustom>
-          <ThemeProviderWrapper>
-            <Navbar />
-            {children}
-            <Footer />
-          </ThemeProviderWrapper>
-        </ThemeProviderCustom>
+        <StyledComponentsRegistry>
+          <ThemeProviderCustom>
+            <ThemeProviderWrapper>
+              <Navbar />
+              {children}
+              <Footer />
+            </ThemeProviderWrapper>
+          </ThemeProviderCustom>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
